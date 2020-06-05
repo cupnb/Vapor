21e3ee9e8a2d033045af19f91c19f4a58aa76cd9
Benjamin Schaab <bennington.schaab@gmail.com>
2020-06-05 13:47:20 +0200

    renamed main.java to Main.java. Added a short test method in main.

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+        test();
+    }
+
+    private static void test() {
+        try {
+            Rawg r = new Rawg();
+            String s = r.searchRequest(5, "Pubg");
+            try {
+                Json j = new Json(s);
+                System.out.println("ALL DONE");
+            }
+            catch (Exception e) {
+                System.out.println(e.getCause());
+            }
+        }
+        catch (Exception e) {
+            System.out.println("ERROR");
+            System.out.println(e.getMessage());
+        }
+    }
+}
