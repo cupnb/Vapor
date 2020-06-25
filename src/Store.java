0160171915b2541e8e442f142cb766e716017927
Benjamin Schaab <bennington.schaab@gmail.com>
2020-06-25 12:29:43 +0200

    finished Store constructor

--- src/Store.java
+++ src/Store.java
@@ -1,4 +1,6 @@
 public class Store extends LibraryObject{
+    int gamesCount;
+    String domain;
 
     public Store(int id) {
         try {
@@ -6,10 +8,23 @@ public class Store extends LibraryObject{
             classString = "store";
             Rawg rawg = new Rawg();
             Json result = new Json(rawg.storeRequest(id));
+            gamesCount = (int) result.getContent("games_count");
+            domain = (String) result.getContent("domain");
+            name = (String) result.getContent("name");
+            description = (String) result.getContent("description");
+            backgroundImage = (String) result.getContent("image_background");
             System.out.println();
         }
         catch (Exception e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    public int getGamesCount() {
+        return gamesCount;
+    }
+
+    public String getDomain() {
+        return domain;
     }
 }
