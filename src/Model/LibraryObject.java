052b7cd9edee70f4bd528036145ce642018b6355
Benjamin Schaab <bennington.schaab@gmail.com>
2020-06-30 15:09:10 +0200

    adapted LibraryObject to work with DataIO

--- src/Model/LibraryObject.java
+++ src/Model/LibraryObject.java
@@ -1,22 +1,15 @@
 package Model;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.awt.image.RenderedImage;
-import java.io.File;
 import java.io.Serializable;
-import java.net.URL;
 
 public abstract class LibraryObject implements Serializable {
     String backgroundImage;
-    boolean isImageLocal;
+    boolean isImagePerm;
     String classString;
     int id;
     String name;
     String description;
-
-    static String resourcePath = "resources";
 
 
     public boolean equals(int id) {
@@ -25,61 +18,21 @@ public abstract class LibraryObject implements Serializable {
 
 
     public void downloadImage() {
-        if(backgroundImage == null || backgroundImage.equals("") || isImageLocal) {
-            return;
-        }
-        try {
-            String destination = String.format("%s/temp/%s_%d.jpg", resourcePath, classString, id);
-            URL url = new URL(backgroundImage);
-            RenderedImage image = ImageIO.read(url);
-            ImageIO.write(image, "JPG", new File(destination));
-            isImageLocal = true;
-            backgroundImage = destination;
-        }
-        catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        DataIO.storeTempImage(id, classString, backgroundImage);
     }
 
 
     public void storeImage() {
-        if(backgroundImage == null || backgroundImage.equals("")) {
-            return;
-        }
-        if(!isImageLocal) {
-            downloadImage();
-        }
-        if(!isImageLocal) {
-            return;
-        }
-        try {
-            String destination = String.format("%s/image/%s/%d.jpg", resourcePath, classString, id);
-            RenderedImage image = ImageIO.read(new File(backgroundImage));
-            ImageIO.write(image, "JPG", new File(destination));
-            backgroundImage = destination;
-        }
-        catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        isImagePerm = DataIO.storePermImage(id, classString, backgroundImage);
     }
 
 
     public BufferedImage loadImage() {
-        if(backgroundImage == null || backgroundImage.equals("")) {
-            return null;
+        if (isImagePerm) {
+            return DataIO.loadPermImage(id, classString, backgroundImage);
         }
-        if(!isImageLocal) {
-            downloadImage();
-        }
-        if(!isImageLocal) {
-            return null;
-        }
-        try {
-            return ImageIO.read(new File(backgroundImage));
-        }
-        catch (Exception e) {
-            System.out.println(e.getMessage());
-            return null;
+        else {
+            return DataIO.loadTempImage(id, classString, backgroundImage);
         }
     }
 
