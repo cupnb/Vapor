b102fc67fc5d1b015ef8b327d6bcce687fe7f043
Noah Boeckmann <noahboeckmann@gmx.net>
2020-07-14 21:23:48 +0200

    wtf

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,12 @@ import Controller.Controller;
 
 public class Main {
 
-    public static void main(String[] args) {
-        test();
+    public static void main(String[] args){
+        try {
+            Controller c = new Controller();
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 
 }
